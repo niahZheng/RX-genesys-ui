11fbe2d5a782a389a001a7dcbae948a7f0b4739b
Crystal Xu <SPIJJX@cathaypacific.com>
2025-07-04 14:38:03 +0800

    Update websockets.ts

--- src/server/websockets.ts
+++ src/server/websockets.ts
@@ -26,3 +26,22 @@ export default function setupWebsockets(app: Express) {
   });
   app.use(proxy);
 }
+
+
+// import {Express} from "express";
+// import {createProxyMiddleware} from "http-proxy-middleware";
+
+// // AAN_SOCKETIO_SERVER has protocol and port inside
+// const ANN_SOCKETIO_SERVER = process.env.ANN_SOCKETIO_SERVER ? process.env.ANN_SOCKETIO_SERVER : "http://localhost:8000";
+
+// export default function setupWebsockets(app: Express) {
+
+//   app.use(
+//     createProxyMiddleware({
+//       pathFilter: "/socket.io",
+//       target: `${ANN_SOCKETIO_SERVER}`,
+//       changeOrigin: true,
+//       ws: true
+//     })
+//   );  
+// }
